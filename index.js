64204c4c6db9bc32abd739ff3c3d302cef6cab75
Tyler Green <tgreen8091@gmail.com>
2017-03-19 00:07:07 +0000

    Switch from async.waterfall to promises

--- index.js
+++ index.js
@@ -5,61 +5,43 @@ const config = require('./scripts/config.json');
 
 const agency_key = config.agencies[0].agency_key;
 const route_id = '16';
+const direction_id = '';
+const system = {};
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUrl);
 
-function createTimetable() {
-  async.series([
-    function(callback) { getRoutes(callback) },
-    function(callback) { getDirections(callback) },
-    function(callback) { getTrips(callback) },
-    function(callback) { getStopTimes(callback) }
-  ], (err) => {
-    if (err) throw err;
+gtfs.getRoutesByAgency(agency_key)
+.then(routes => {
+  //console.log('routes',routes);
+  return gtfs.getDirectionsByRoute(agency_key, route_id);
+})
+.then(directions => {
+  //console.log('directions',directions);
+  system.directions = {};
+  system.directions[route_id] = directions.map(direction => direction.trip_headsign);
+  return gtfs.getTripsByRouteAndDirection(agency_key, route_id, direction_id);
+})
+// Partition the list of trips into headsigns
+.then(trips => {
+  //console.log('trips',trips);
+  
+  return Promise.all(system.directions[route_id].map(direction => {
+    let tripList = trips.filter(trip => trip.trip_headsign === direction);
     
-    process.exit();
+    //return new Promise((resolve, reject) => {
+    return Promise.all(tripList.map(trip => {
+      return gtfs.getStoptimesByTrip(agency_key, trip.trip_id);
+    }));
+  }));
+})
+.then(tripData => {
+  tripData[0].forEach(trip => {
+    let times = trip.map(stoptime => { return stoptime.arrival_time });
+    console.log(times.join(','));
   });
-}
-
-function getRoutes(callback) {
-  gtfs.getRoutesByAgency(agency_key, (err, routes) => {
-    if (err) throw err;
-    
-    const ids = routes.map(route => route.route_id);
-    
-    console.log('routes', ids);
-    callback();
-  });
-}
-
-function getDirections(callback) {
-  console.log('id',route_id);
-  gtfs.getDirectionsByRoute(agency_key, route_id, (err, directions) => {
-    if (err) throw err;
-    console.log('directions',directions);
-    callback();
-  });
-}
-
-function getTrips(callback) {
-  const direction_id = '';
-  gtfs.getTripsByRouteAndDirection(agency_key, route_id, direction_id, (err, trips) => {
-    if (err) throw err;
-    
-    console.log('trips',trips);
-    
-    callback();
-  });
-}
-
-function getStopTimes(callback) {
-  const trip_id = '114266020';
-  gtfs.getStoptimesByTrip(agency_key, trip_id, (err, stoptimes) => {
-    if (err) throw err; 
-    console.log('stop times',stoptimes.map(time => time.arrival_time));
-    callback();
-  });
-}
-
-createTimetable();
+})
+.catch(console.log.bind(console))
+.then(() => {
+  process.exit();
+});
