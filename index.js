ae80eba98d28d63e3bf0acb2c0c1ebddf9709861
Tyler Green <tgreen8091@gmail.com>
2017-04-03 01:12:14 +0000

    Initial update to use async/await

--- index.js
+++ index.js
@@ -11,64 +11,67 @@ const system = {};
 mongoose.Promise = global.Promise;
 mongoose.connect(config.mongoUrl);
 
-gtfs.getStops(agency_key)
-.then(stops => {
+const saveStops = (stops) => {
   system.stops = stops.reduce(function(obj, stop, i) {
     obj[stop.stop_id] = stop;
     return obj;
   }, {});
-  
-  return gtfs.getRoutesByAgency(agency_key);
-})
-.then(routes => {
-  //console.log('routes',routes);
-  return gtfs.getDirectionsByRoute(agency_key, route_id);
-})
-.then(directions => {
-  //console.log('directions',directions);
-  system.directions = {};
-  system.directions[route_id] = directions.map(direction => direction.trip_headsign);
-  return gtfs.getTripsByRouteAndDirection(agency_key, route_id, direction_id);
-})
-// Partition the list of trips into headsigns
-.then(trips => {
-  //console.log('trips',trips.filter(trip => { return trip.trip_id === '114283020' || trip.trip_id === '114283030' }));
+};
+
+const saveTrips = (trips) => {
   system.trips = trips.reduce(function(obj, trip, i) {
     obj[trip.trip_id] = trip;
     return obj;
   }, {});
-  
-  return Promise.all(system.directions[route_id].map(direction => {
-    let tripList = trips.filter(trip => trip.trip_headsign === direction);
-    
-    //return new Promise((resolve, reject) => {
-    return Promise.all(tripList.map(trip => {
-      return gtfs.getStoptimesByTrip(agency_key, trip.trip_id);
-    }));
-  }));
-})
-.then(tripData => {
-  let output = '';
-  //console.log('time',tripData[0][0]);
-  tripData[0].forEach(trip => {
-    let names = trip.map(stoptime => { return system.stops[stoptime.stop_id].stop_name });
-    output += `${trip[0].trip_id},${system.trips[trip[0].trip_id].service_id},${names.join(',')}\n`;
-    let times = trip.map(stoptime => { return stoptime.arrival_time });
-    output += `${trip[0].trip_id},${system.trips[trip[0].trip_id].service_id},${times.join(',')}\n`;
-  });
+};
 
-  const filename = `${route_id}.csv`;
-  return new Promise(function(resolve, reject) {
-    fs.writeFile(filename, output, function(err) {
-      if (err) reject(err);
-      else resolve();
+const saveDirections = (directions, route_id) => {
+  system.directions = {};
+  system.directions[route_id] = directions.map(direction => direction.trip_headsign);
+  //system.directions = directions.reduce((obj, direction, i) => {
+  //  obj[direction.trip_headsign] = direction;
+  //  return obj;
+  //}, {});
+};
+
+const makeTable = async (route_id) => {
+  try {
+    const stops = await gtfs.getStops(agency_key);
+    saveStops(stops);
+    const routes = await gtfs.getRoutesByAgency(agency_key);
+    const directions = await gtfs.getDirectionsByRoute(agency_key, route_id);
+    saveDirections(directions, route_id);
+    const trips = await gtfs.getTripsByRouteAndDirection(agency_key, route_id, direction_id);
+    saveTrips(trips);
+    
+    Promise.all(system.directions[route_id].map(direction => {
+        let tripList = trips.filter(trip => trip.trip_headsign === direction);
+        
+        return Promise.all(tripList.map(trip => {
+          return gtfs.getStoptimesByTrip(agency_key, trip.trip_id);
+        }));
+    }))
+    .then(tripData => {
+      let output = '';
+      tripData[0].forEach(trip => {
+        let names = trip.map(stoptime => { return system.stops[stoptime.stop_id].stop_name });
+        output += `${trip[0].trip_id},${system.trips[trip[0].trip_id].service_id},${names.join(',')}\n`;
+        let times = trip.map(stoptime => { return stoptime.arrival_time });
+        output += `${trip[0].trip_id},${system.trips[trip[0].trip_id].service_id},${times.join(',')}\n`;
+      });
+    
+      const filename = `${route_id}.csv`;
+      return new Promise(function(resolve, reject) {
+        fs.writeFile(filename, output, function(err) {
+          if (err) reject(err);
+          else resolve();
+        });
+      });
     });
-  });
-})
-.then(none => {
-   console.log('saved'); 
-})
-.catch(console.log.bind(console))
-.then(() => {
-  process.exit();
-});
+    
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+makeTable(route_id);
